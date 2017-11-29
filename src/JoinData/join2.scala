ec8e455a3bc02966cd11f63c76cddf3d37b577a4
huangxi <huangxi183@gmail.com>
2017-11-29 16:20:51 -0500

    add cache in kmeans

--- src/JoinData/join2.scala
+++ src/JoinData/join2.scala
@@ -2,25 +2,30 @@
 
 import org.apache.spark.sql.SQLContext
 
-val sqlContext = new SQLContext(sc)
+object Join2 {
+  def main(args: Array[String]): Unit = {
 
-val crime_price_dataset = sqlContext.read
-  .load("project/data/JoinedData/*")
+    val sqlContext = new SQLContext(sc)
 
-val people_dataset = sqlContext.read
-  .format("com.databricks.spark.csv")
-  .option("header", "true")
-  .option("inferSchema", "true")
-  .load("project/data/demographic.csv")
+    val crime_price_dataset = sqlContext.read
+      .load("project/data/JoinedData/*")
 
-
-val df_price = crime_price_dataset.as("dfprice")
-
-val df_people = people_dataset.as("dfpeople")
-
-val join_df = df_price.join(df_people, col("dfprice.CountyName") === col("dfpeople.county") && col("dfprice.State") === col("dfpeople.state"),"inner")
-  .select("dfprice.RegionName", "dfprice.City", "dfprice.State", "dfprice.CountyName", "dfprice.2017-09", "dfprice.Crime_Rate_Per_100000",
-  "dfpeople.male", "dfpeople.female","dfpeople.white", "dfpeople.black", "dfpeople.asian", "dfpeople.hispanic")
+    val people_dataset = sqlContext.read
+      .format("com.databricks.spark.csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("project/data/demographic.csv")
 
 
-join_df.write.save("project/data/JoinedData2")
+    val df_price = crime_price_dataset.as("dfprice")
+
+    val df_people = people_dataset.as("dfpeople")
+
+    val join_df = df_price.join(df_people, col("dfprice.CountyName") === col("dfpeople.county") && col("dfprice.State") === col("dfpeople.state"), "inner")
+      .select("dfprice.RegionName", "dfprice.City", "dfprice.State", "dfprice.CountyName", "dfprice.2017-09", "dfprice.Crime_Rate_Per_100000",
+        "dfpeople.male", "dfpeople.female", "dfpeople.white", "dfpeople.black", "dfpeople.asian", "dfpeople.hispanic")
+
+
+    join_df.write.save("project/data/JoinedData2")
+  }
+}
--- src/Kmeans/kmeans.scala
+++ src/Kmeans/kmeans.scala
@@ -3,28 +3,40 @@ import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.sql.SQLContext
 
-val sqlContext = new SQLContext(sc)
 
-// Load and parse the data
-val datas = sqlContext.read
-  .load("project/data/JoinedData2/*")
+object Kmeans {
+  def main(args: Array[String]): Unit = {
+    val sqlContext = new SQLContext(sc)
 
+    // Load and parse the data
+    val datas = sqlContext.read
+      .load("project/data/JoinedData2/*")
 
-val selectedData = datas.select("2017-09", "Crime_Rate_Per_100000", "male", "female", "white", "black", "asian", "hispanic")
-val rdd = selectedData.map(x=> Vectors.dense(x.getDouble(0),x.getDouble(1), x.getDouble(2), x.getDouble(3),x.getDouble(4), x.getDouble(5), x.getDouble(6), x.getDouble(7) ))
+    val selectedData = datas.select("2017-09", "Crime_Rate_Per_100000", "male", "female", "white", "black", "asian", "hispanic")
+    val rdd = selectedData.map(x => Vectors.dense(
+      x.getDouble(0),
+      x.getDouble(1),
+      x.getDouble(2),
+      x.getDouble(3),
+      x.getDouble(4),
+      x.getDouble(5),
+      x.getDouble(6),
+      x.getDouble(7))).cache()
 
-//val data = sc.textFile("data/mllib/kmeans_data.txt")
-//val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
+    //val data = sc.textFile("data/mllib/kmeans_data.txt")
+    //val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
 
-// Cluster the data into two classes using KMeans
-val numClusters = 100
-val numIterations = 20
-val clusters = KMeans.train(rdd, numClusters, numIterations)
+    // Cluster the data into two classes using KMeans
+    val numClusters = 1000
+    val numIterations = 20
+    val clusters = KMeans.train(rdd, numClusters, numIterations)
 
-// Evaluate clustering by computing Within Set Sum of Squared Errors
-val WSSSE = clusters.computeCost(rdd)
-println("Within Set Sum of Squared Errors = " + WSSSE)
+    // Evaluate clustering by computing Within Set Sum of Squared Errors
+    val WSSSE = clusters.computeCost(rdd)
+    println("Within Set Sum of Squared Errors = " + WSSSE)
 
-// Save and load model
-clusters.save(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
-val sameModel = KMeansModel.load(sc, "target/org/apache/spark/KMeansExample/KMeansModel")
+    // Save and load model
+    clusters.save(sc, "project/data/output/KMeansModel")
+    val sameModel = KMeansModel.load(sc, "project/data/output/KMeansModel")
+  }
+}
